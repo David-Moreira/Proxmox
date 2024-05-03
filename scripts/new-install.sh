af6d1934c19d6bd24a179e22172c4e1af19a1b6f
David Moreira <david_nogueira_moreira@hotmail.com>
2024-05-03 18:52:11 +0100

    test

--- scripts/new-install.sh
+++ scripts/new-install.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
-# source <(curl -s https://raw.githubusercontent.com/David-Moreira/Proxmox/scripts/misc/build.func)
+source <(curl -s https://raw.githubusercontent.com/David-Moreira/Proxmox/scripts/misc/build.func)
 
 echo -e "Testing... Loading..."
+
+msg_info "Testing..."
+msg_ok "Testing ok..."
+msg_error "Testing Error..."
